5672988400eb40a36acab960ca3848ab5b917591
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-08-29 13:54:41 +0200

    addEmployeTest

--- src/test/java/org/Kader/TestMockitoMockitoMvcApplicationTests.java
+++ src/test/java/org/Kader/TestMockitoMockitoMvcApplicationTests.java
@@ -1,15 +1,26 @@
 package org.Kader;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.Kader.model.Employe;
+import org.Kader.model.Response;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,16 +31,27 @@ public class TestMockitoMockitoMvcApplicationTests {
 	@Autowired
 	private WebApplicationContext context;
 	
+	private ObjectMapper om=new ObjectMapper();
+	
 	@Before
 	private void setUp() {
 		mockMvc=MockMvcBuilders.webAppContextSetup(context).build();
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Test
-	public void addEmployeTest() {
+	public void addEmployeTest() throws Exception {
+		Employe employe=new Employe();
+		employe.setName("Yahyaoui");
+		employe.setDept("IT/Imformatique");
+		String jsonRequest=om.writeValueAsString(employe);
 		
-		
-		
+		MvcResult result=mockMvc.perform(post("/employe/addEmploye").content(jsonRequest)
+														   .content(MediaType.APPLICATION_JSON_VALUE))
+														   .andExpect(status().isOk()).andReturn();
+		String resultContent=result.getResponse().getContentAsString();
+		Response reponse=om.readValue(resultContent, Response.class);
+		Assert.assertTrue(reponse.isSuccess()==Boolean.TRUE);
 	}
 
 }
